b9d6cbc9609d895a9b9668acd48ee844a032af1e
tmousecello <59406455+tmousecello@users.noreply.github.com>
2021-06-07 00:52:47 +0800

    Update random forest test.py
    the code write on 6/6&6/7

--- random forest test.py
+++ random forest test.py
@@ -13,6 +13,7 @@ tree.fit(iris.data, iris.target)
 figure(figsize=(16,9),dpi=68)
 plot_tree(tree, feature_names=iris.feature_names)
 
+
 import random
 
 def bootstrap(x,y):
@@ -26,9 +27,28 @@ def bootstrap(x,y):
 
 tree = DecisionTreeClassifier(max_depth=2) #tree = DecisionTreeClassifier(max_depth=2，random_state=0)可決定隨機
 
-x, y = bootstrap(iris.data，iris.target)
-tree.fit(xㄝy)
+x, y = bootstrap(iris.data, iris.target)
+tree.fit(x,y)
 
 figure(figsize=(16, 9),dpi=68)
 plot_tree(tree, feature_names=iris.feature_names)
 
+
+import random
+
+def bootstrap(x,y):
+  n = len(x)
+  assert len(y) == n
+  #random.seed(0) #控制隨機
+  indices = random.choices(range(n),k=n) 
+  indices = set(indices) #轉成集合(不重複資料)
+  indices = list(indices)
+  return x[indices], y[indices]
+
+tree = DecisionTreeClassifier(max_depth=2) #tree = DecisionTreeClassifier(max_depth=2，random_state=0)可決定隨機
+
+x, y = bootstrap(iris.data, iris.target)
+tree.fit(x,y)
+
+figure(figsize=(16, 9),dpi=68)
+plot_tree(tree, feature_names=iris.feature_names)
