cb9f6e89ee2f339aeca4ace8e6c6f491537d5b76
tmousecello <59406455+tmousecello@users.noreply.github.com>
2021-06-06 23:56:53 +0800

    Create random forest test.py
    the code write on 6/6

---
+++ random forest test.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import load_iris
+from pprint import pprint
+iris = load_iris()
+pprint(iris)
+
+
+from matplotlib.pyplot import figure
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+tree = DecisionTreeClassifier(max_depth=2)
+tree.fit(iris.data, iris.target)
+
+figure(figsize=(16,9),dpi=68)
+plot_tree(tree, feature_names=iris.feature_names)
+
+import random
+
+def bootstrap(x,y):
+  n = len(x)
+  assert len(y) == n
+  #random.seed(0) #控制隨機
+  indices = random.choices(range(n),k=n) 
+  indices = set(indices) #轉成集合(不重複資料)
+  indices = list(indices)
+  return x[indices], y[indices]
+
+tree = DecisionTreeClassifier(max_depth=2) #tree = DecisionTreeClassifier(max_depth=2，random_state=0)可決定隨機
+
+x, y = bootstrap(iris.data，iris.target)
+tree.fit(xㄝy)
+
+figure(figsize=(16, 9),dpi=68)
+plot_tree(tree, feature_names=iris.feature_names)
+
